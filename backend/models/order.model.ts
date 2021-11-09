5479e74a3bdab964a8cdd23381134de268736041
Tomás Catena <tomascatena@gmail.com>
2021-11-09 22:08:53 +0100

    format models

--- backend/models/order.model.ts
+++ backend/models/order.model.ts
@@ -9,10 +9,26 @@ const orderSchema = new mongoose.Schema(
     },
     orderItems: [
       {
-        name: { type: String, required: true, trim: true },
-        quantity: { type: Number, required: true, default: 0 },
-        image: { type: String, required: true, trim: true },
-        price: { type: Number, required: true, default: 0 },
+        name: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 0,
+        },
+        image: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+          default: 0,
+        },
         product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
@@ -21,25 +37,85 @@ const orderSchema = new mongoose.Schema(
       },
     ],
     shippingAddress: {
-      address: { type: String, required: true, trim: true },
-      city: { type: String, required: true, trim: true },
-      postalCode: { type: String, required: true, trim: true },
-      country: { type: String, required: true, trim: true },
+      address: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      city: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      country: {
+        type: String,
+        required: true,
+        trim: true,
+      },
     },
-    paymentMethod: { type: String, required: true, trim: true },
+    paymentMethod: {
+      type: String,
+      required: true,
+      trim: true,
+    },
     paymentResult: {
-      id: { type: String, required: true, trim: true },
-      status: { type: String, required: true, trim: true },
-      update_time: { type: String, required: true, trim: true },
-      email_address: { type: String, required: true, trim: true },
+      id: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      status: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      update_time: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      email_address: {
+        type: String,
+        required: true,
+        trim: true,
+      },
     },
-    taxPrice: { type: Number, required: true, default: 0.0 },
-    shippingPrice: { type: Number, required: true, default: 0.0 },
-    totalPrice: { type: Number, required: true, default: 0.0 },
-    isPaid: { type: Boolean, required: true, default: false },
-    paidAt: { type: Date },
-    isDelivered: { type: Boolean, required: true, default: false },
-    deliveredAt: { type: Date },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
   },
   { timestamps: true }
 );
--- backend/models/user.model.ts
+++ backend/models/user.model.ts
@@ -4,7 +4,11 @@ import { roles } from '../config/roles';
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, trim: true },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
     email: {
       type: String,
       required: true,
@@ -30,8 +34,15 @@ const userSchema = new mongoose.Schema(
         }
       },
     },
-    role: { type: String, enum: roles, default: 'user' },
-    isEmailVerified: { type: Boolean, default: false },
+    role: {
+      type: String,
+      enum: roles,
+      default: 'user',
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
