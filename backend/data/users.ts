a5d9d14a4f3cfb30fe0c461e5813c7e51f3abbf1
Tomás Catena <tomascatena@gmail.com>
2021-11-11 19:18:40 +0100

    Prepare sample data for seeder

---
+++ backend/data/users.ts
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs';
+
+export const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('12345', 12),
+    role: 'admin',
+  },
+  {
+    name: 'Pelusa Atun',
+    email: 'pelusa@example.com',
+    password: bcrypt.hashSync('12345', 12),
+    role: 'user',
+  },
+  {
+    name: 'Rufo Klus',
+    email: 'rufo@example.com',
+    password: bcrypt.hashSync('12345', 12),
+    role: 'user',
+  },
+];
